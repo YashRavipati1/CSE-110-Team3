1da0f11823cf05f05c6c2e696e6a6309d664de49
Yashwanth Ravipati <yashravipatischool@gmail.com>
2024-11-15 03:06:04 -0800

    Small changes to login tests

--- frontend/React-App/src/Login.test.tsx
+++ frontend/React-App/src/Login.test.tsx
@@ -1,9 +1,12 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
-import App from './App';
+import { Login } from './pages/Login';
+import { BrowserRouter } from 'react-router-dom';
 
 test('renders login button', () => {
-  render(<App />);
+  render(<BrowserRouter>
+    <Login />
+  </BrowserRouter>);
   const linkElement = screen.getByText(/Sign in with Google/i);
   expect(linkElement).toBeInTheDocument();
 });
