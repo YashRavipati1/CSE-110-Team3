8e79e80e834d9b33a9f4dffc602029e16b57305d
Zere-m <146875320+Zere-m@users.noreply.github.com>
2024-11-20 16:22:07 -0800

    commiting meal card component

--- frontend/React-App/src/components/MealCard.tsx
+++ frontend/React-App/src/components/MealCard.tsx
@@ -3,7 +3,6 @@ import Button from '../components/Button';
 import '../css_styling_files/mealCard.css';
 // Import Meal interface in future
 interface Meal {
-<<<<<<< HEAD
     id: string;
     name: string;
     type: string;
@@ -20,19 +19,6 @@ interface MealCardProps {
     meal: Meal;
     onEdit: (id: string) => void;
     onDelete: (id: string) => void;
-=======
-    id: number;
-    name: string;
-    type: string;
-    calories: number;
-    macros: string[];
-}
-
-interface MealCardProps {
-    meal: Meal;
-    onEdit: () => void;
-    onDelete: () => void;
->>>>>>> b555d2b19299bfda5d64a59e7aa07077970cbfb0
 }
 
 const MealCard: React.FC<MealCardProps> = ({ meal, onEdit, onDelete }) => {
@@ -42,19 +28,11 @@ const MealCard: React.FC<MealCardProps> = ({ meal, onEdit, onDelete }) => {
                 <h3>{meal.name}</h3>
                 <p>Type: {meal.type}</p>
                 <p>Calories: {meal.calories}</p>
-<<<<<<< HEAD
                 <span>Protein: {meal.protein} Carbs: {meal.carbs} Fats: {meal.fats}</span>
             </div>
             <div className="meal-actions">
                 <Button icon="edit" text="Edit Meal" size = "medium" color = "#298e46" onClick = {()=> onEdit(meal.id)}/>
                 <Button icon="delete" text="" size = "medium" color = "#298e46"onClick = {()=> onDelete(meal.id)}className="delete-button" />
-=======
-                <p>Macros: {meal.macros.join(', ')}</p>
-            </div>
-            <div className="meal-actions">
-                <Button icon="edit" text="Edit Meal" size = "medium" color = "#298e46" onClick={onEdit}/>
-                <Button icon="delete" text="" size = "medium" color = "#298e46" onClick={onDelete} className="delete-button" />
->>>>>>> b555d2b19299bfda5d64a59e7aa07077970cbfb0
             </div>
         </div>
     );
